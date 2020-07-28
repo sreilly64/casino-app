70e48f8e5e2d88c325915eed73987c341e8c63a3
Shane Reilly <sreilly@zipcoders-MacBook-Pro.local>
2020-07-28 17:44:56 -0400

    add methods to interfaces and abstract classes

--- src/main/java/io/zipcoder/casino/player/GamblingPlayer.java
+++ src/main/java/io/zipcoder/casino/player/GamblingPlayer.java
@@ -1,4 +1,8 @@
 package io.zipcoder.casino.player;
 
 public interface GamblingPlayer {
+
+    Integer getCurrentFunds();
+
+    void addToCurrentFunds(Integer amount);
 }
--- src/main/java/io/zipcoder/casino/player/Player.java
+++ src/main/java/io/zipcoder/casino/player/Player.java
@@ -1,4 +1,24 @@
 package io.zipcoder.casino.player;
 
 public class Player implements GamblingPlayer {
+
+    private String name;
+    private Integer currentFunds;
+
+    public Player(String name, Integer initialFunds) {
+        this.name = name;
+        this.currentFunds = initialFunds;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Integer getCurrentFunds() {
+        return this.currentFunds;
+    }
+
+    public void addToCurrentFunds(Integer amount) {
+        this.currentFunds += amount;
+    }
 }
