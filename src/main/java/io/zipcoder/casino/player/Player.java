15e199c9a3e49ddf75b262ab87d416858582b4db
lakegreene <lakegreene@yahoo.com>
2020-07-28 19:55:55 -0400

    Update Player class

--- src/main/java/io/zipcoder/casino/player/Player.java
+++ src/main/java/io/zipcoder/casino/player/Player.java
@@ -1,4 +1,11 @@
 package io.zipcoder.casino.player;
 
 public class Player implements GamblingPlayer {
+    private String name;
+    public Player(String name, Integer startingFunds) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
 }
