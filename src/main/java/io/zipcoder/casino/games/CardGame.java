619a9e3f7d0c6e058e085efacaf75a76216012e2
jyothi14691 <jyothi14691@gmail.com>
2020-07-28 19:31:06 -0400

    Added Card and Card game

--- src/main/java/io/zipcoder/casino/games/CardGame.java
+++ src/main/java/io/zipcoder/casino/games/CardGame.java
@@ -1,6 +1,32 @@
 package io.zipcoder.casino.games;
 
+import io.zipcoder.casino.Card;
 import io.zipcoder.casino.games.Game;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public abstract class CardGame implements Game {
+
+    List<Card> currentDeck = new ArrayList<Card>();
+
+    public CardGame(List<Card> currentDeck) {
+        this.currentDeck = currentDeck;
+    }
+
+     public Card drawTopCard()
+     {
+         Card topCard=null;
+         if(currentDeck != null && !currentDeck.isEmpty()){
+            topCard = currentDeck.remove(0);
+         }
+         return topCard;
+     }
+
+     abstract void dealCards(Integer numOfCards);
+
+     abstract public void getWinner();
+
+
 }
--- src/main/java/io/zipcoder/casino/games/GoFish.java
+++ src/main/java/io/zipcoder/casino/games/GoFish.java
@@ -1,4 +1,32 @@
 package io.zipcoder.casino.games;
 
+import io.zipcoder.casino.Card;
+import io.zipcoder.casino.player.Player;
+
+import java.util.List;
+
 public class GoFish extends CardGame {
+    public GoFish(List<Card> currentDeck) {
+        super(currentDeck);
+    }
+
+    void dealCards(Integer numOfCards) {
+
+    }
+
+    public void resetGame() {
+
+    }
+
+    public void quitGame() {
+
+    }
+
+    public String getGameName() {
+        return null;
+    }
+
+    public void startGame(Player player) {
+
+    }
 }
