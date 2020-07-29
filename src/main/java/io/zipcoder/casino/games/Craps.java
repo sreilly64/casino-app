7640471e656b35643dfc87ba1fb22787f42dc78c
Shane Reilly <sreilly@zipcoders-MacBook-Pro.local>
2020-07-29 12:41:11 -0400

    created ui, mostly

--- src/main/java/io/zipcoder/casino/games/Craps.java
+++ src/main/java/io/zipcoder/casino/games/Craps.java
@@ -1,8 +1,10 @@
 package io.zipcoder.casino.games;
 
 import io.zipcoder.casino.player.Player;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Craps extends DiceGame implements GamblingGame{
@@ -13,7 +15,6 @@ public class Craps extends DiceGame implements GamblingGame{
     private ArrayList<Integer> diceRolls;
     private Integer betAmount;
     private Player player;
-    private CrapsGUI ui;
 
     public enum BetType {PASS, PASS_ODDS, DONT_PASS, DONT_PASS_ODDS, COME,COME_4, COME_5, COME_6, COME_8, COME_9,
         COME_10, COME_ODDS_4, COME_ODDS_5, COME_ODDS_6, COME_ODDS_8, COME_ODDS_9, COME_ODDS_10, DONT_COME, DONT_COME_4,
@@ -26,7 +27,38 @@ public class Craps extends DiceGame implements GamblingGame{
     public Craps() {
         this.comOutPhase = true;
         this.currentPoint = 0;
-        //this.ui = new CrapsGUI(this);
+        //this.startGame();
+        setUpBetsMap();
+    }
+
+    private void setUpBetsMap() {
+        Map<BetType, Integer> output = new LinkedHashMap<BetType, Integer>();
+        for(BetType bet: BetType.values()){
+            output.put(bet, 0);
+        }
+        currentBets = output;
+    }
+
+    public Boolean getGamePhase(){
+        return this.comOutPhase;
+    }
+
+    public Integer getCurrentPoint(){
+        return this.currentPoint;
+    }
+
+    public void setLastDiceRoll(){
+        this.diceRolls = rollDice(2);
+    }
+
+    public Integer calculatePayoffs(){
+
+        return null;
+    }
+
+    public BetType selectBetType(String betType){
+
+        return null;
     }
 
     public void setPlayer(Player player){
@@ -34,7 +66,16 @@ public class Craps extends DiceGame implements GamblingGame{
     }
 
     public Integer getCurrentBet() {
+
         return null;
+    }
+
+    public Integer getBetAmount() {
+        return betAmount;
+    }
+
+    public void setBetAmount(Integer betAmount) {
+        this.betAmount = betAmount;
     }
 
     public void bet(Integer amount) {
@@ -56,6 +97,7 @@ public class Craps extends DiceGame implements GamblingGame{
     public void quitGame() {
         clearBets();
         //shutdown javafx
+
     }
 
     public String getGameName() {
@@ -65,6 +107,6 @@ public class Craps extends DiceGame implements GamblingGame{
     public void startGame(Player player) {
         setPlayer(player);
         //initiate javafx
-        this.ui = new CrapsGUI(this);
+        //CrapsGUI.main(null);
     }
 }
